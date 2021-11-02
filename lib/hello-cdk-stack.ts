21d0102d3d16073b66c0303d459e78cad57d4069
NaumanMunir9 <naumanmunir9@gmail.com>
2021-11-02 10:41:28 +0500

    initial commit

--- lib/hello-cdk-stack.ts
+++ lib/hello-cdk-stack.ts
@@ -1,9 +1,4 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core";
+import * as s3 from "@aws-cdk/aws-s3";
 
-export class HelloCdkStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
-
-    // The code that defines your stack goes here
-  }
-}
+export class HelloCdkStack extends cdk.Stack {}
